821a2838c9bd77f9223c04c89fabfa3ea82f4f8b
Aaron Jevon Benedict Kongdoh <148040924+trfyrt@users.noreply.github.com>
2024-10-04 17:24:18 +0800

    minor fix

--- Lab_exercise_2_Aaron Kongdoh/function_arrays.php
+++ Lab_exercise_2_Aaron Kongdoh/function_arrays.php
@@ -1,6 +1,12 @@
 <?php
 include 'variables.php';
-ob_clean();
+
+$mahasiswa1 = new Mahasiswa("Aaron Jevon Benedict Kongdoh", '2006-01-08', "male", 4.0);
+$mahasiswa2 = new Mahasiswa("Gilbert Winardy", '2004-01-08', "male", 4.0);
+$mahasiswa3 = new Mahasiswa("Deny Wahyudi Asaloei", '2005-03-15', "male", 4.0);
+$mahasiswa4 = new Mahasiswa("Aryo Karel Merentek", '2004-01-04', "male", 4.0);
+$mahasiswa5 = new Mahasiswa("Hainzel Kemal", '2005-05-25', "male", 4.0);
+
 
 function greet($name){
     echo "Hello $name!";
@@ -8,8 +14,8 @@ function greet($name){
 
 $students = array($mahasiswa1->name, $mahasiswa2->name, $mahasiswa3->name, $mahasiswa4->name, $mahasiswa5->name);
 
-foreach ($students as $x) {
-    greet($x);
+for ($i=0; $i<count($students) ;$i++) {
+    greet($students[$i]);
     echo '<br>';
   }
 ?>
--- Lab_exercise_2_Aaron Kongdoh/variables.php
+++ Lab_exercise_2_Aaron Kongdoh/variables.php
@@ -4,7 +4,7 @@ class Mahasiswa {
     public $name;
     public $birthDate;
     public $age;
-    public $sex;
+    public $sex = "";
     public $gpa;
     public $isStudent = TRUE;
 
@@ -20,17 +20,5 @@ class Mahasiswa {
         $this->gpa = $newGpa;
         }
 }
-
-$mahasiswa1 = new Mahasiswa("Aaron Jevon Benedict Kongdoh", '2006-01-08', "male", 4.0);
-$mahasiswa2 = new Mahasiswa("Gilbert Winardy", '2004-01-08', "male", 4.0);
-$mahasiswa3 = new Mahasiswa("Deny Wahyudi Asaloei", '2005-03-15', "male", 4.0);
-$mahasiswa4 = new Mahasiswa("Aryo Karel Merentek", '2004-01-04', "male", 4.0);
-$mahasiswa5 = new Mahasiswa("Hainzel Kemal", '2005-05-25', "male", 4.0);
-
-echo "<b>Mahasiswa 1</b><br>Name: $mahasiswa1->name <br>Age: $mahasiswa1->age<br>Sex: $mahasiswa1->sex<br>GPA: $mahasiswa1->gpa<br><br>";
-echo "<b>Mahasiswa 2</b><br>Name: $mahasiswa2->name <br>Age: $mahasiswa2->age<br>Sex: $mahasiswa2->sex<br>GPA: $mahasiswa2->gpa<br><br>";
-echo "<b>Mahasiswa 3</b><br>Name: $mahasiswa3->name <br>Age: $mahasiswa3->age<br>Sex: $mahasiswa3->sex<br>GPA: $mahasiswa3->gpa<br><br>";
-echo "<b>Mahasiswa 4</b><br>Name: $mahasiswa4->name <br>Age: $mahasiswa4->age<br>Sex: $mahasiswa4->sex<br>GPA: $mahasiswa4->gpa<br><br>";
-echo "<b>Mahasiswa 5</b><br>Name: $mahasiswa5->name <br>Age: $mahasiswa5->age<br>Sex: $mahasiswa5->sex<br>GPA: $mahasiswa5->gpa<br><br>";
 
 ?>
